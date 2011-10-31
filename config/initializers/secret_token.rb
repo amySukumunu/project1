dc0abd212b9a41983aa443d22cc3fe6d3802bda2
amySukumunu <amysukumunu@mmu-cyber-10.112.6.149.cyber.mmu.edu.my>
2011-11-01 06:47:08 +0800

    Chapter 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project1::Application.config.secret_token = '08cccdc67c3baf08aa20fdbe685faf41e0ab78ee830e339107c669d2314e5ecee290c75623725305df5d85ba04cc5723274aa2ef6d379d6ecedf7d5425b18672'
